b5577ea446b33752abe7753e36e514db82426c07
maettuu <mangori41@bluewin.ch>
2021-05-01 20:14:01 +0200

    neighbors not defined for player -> order array failing

--- src/components/game/Lobby.js
+++ src/components/game/Lobby.js
@@ -30,7 +30,7 @@ function Lobby({currUser, currPlayer_table, updatePlayer_table}) {
     const [currPlayer, setCurrPlayer] = useState();
     const [count, setCount] = useState(0);
     const [loopvar, setLoopvar] = useState(true);
-    const [playeramount, setPlayeramount] = useState(4/*currPlayer_table.players.length*/);
+    const [playeramount, setPlayeramount] = useState(currPlayer_table.players.length);
     const [toomanycards, setToomanycards] = useState("loading");//TODO uncomment this
     const interval = useInterval(async () => {    
 //repeating requests to keep player_table and player up to date
@@ -44,7 +44,7 @@ function Lobby({currUser, currPlayer_table, updatePlayer_table}) {
         currPlayer_table = new PlayerTable(playertable_response.data);
         updatePlayer_table(currPlayer_table);
         localStorage.setItem('player_table', JSON.stringify(currPlayer_table));
-        correctOrder();
+        //correctOrder();
         setToomanycards(currPlayer.hand.playCards.length-currPlayer.bullets);
 
 
@@ -70,26 +70,21 @@ function Lobby({currUser, currPlayer_table, updatePlayer_table}) {
         }
         
         setCount(count + 1);  }, 5000);
-    
 
     useEffect(async () => {
-        const userData = JSON.parse(localStorage.getItem('player_table'));
-        if (userData == null) {
-            return
-        }
-        const currentPlayer_table = new PlayerTable(userData);
-        updatePlayer_table(currentPlayer_table);
-        console.log(currentPlayer_table);
-
-        const response = await authApi().get(`/games/${currPlayer_table.id}/players/${currUser.id}`);
-        let currpl = new PlayerModel(response.data);
-        setCurrPlayer(response.data);
-        //localStorage.setItem('player', JSON.stringify(currPlayer));
-        console.log(currPlayer_table);
-        correctOrder();
-        console.log(orderArray);
         try {
+            const userData = JSON.parse(localStorage.getItem('player_table'));
+            if (userData == null) {
+                return
+            }
+            const currentPlayer_table = new PlayerTable(userData);
+            updatePlayer_table(currentPlayer_table);
 
+            const response = await authApi().get(`/games/${currPlayer_table.id}/players/${currUser.id}`);
+            let currpl = new PlayerModel(response.data);
+            setCurrPlayer(response.data);
+            //localStorage.setItem('player', JSON.stringify(currPlayer));
+            correctOrder();
         } catch (error) {
             alert(`Something went wrong while fetching the users: \n${handleError(error)}`);
         }
@@ -169,20 +164,16 @@ function Lobby({currUser, currPlayer_table, updatePlayer_table}) {
     const [orderArray, setOrderArray] = useState([]);
     
 
-    function correctOrder(){
-        const current_array = [];
+    async function correctOrder(){
+        let current_array = [];
         for (let i=0; i<currPlayer_table.players.length; i++) {
-            //console.log(currPlayer_table.players[i].id);
-            console.log(currUser.id);
-            if(currUser.id == currPlayer_table.players[i].id) {
-                current_array.push(currPlayer);
-                console.log(current_array[i]);
+            if(currUser.id === currPlayer_table.players[i].id) {
+                current_array[0] = currPlayer;
             }
         }
         for (let i = 0; i < playeramount - 1; i++) {
-            current_array[i].rightNeighbor = current_array[i+1];
+            current_array[i+1] = current_array[i].rightNeighbor;
         }
-
         setOrderArray(current_array);
     }
 
@@ -297,110 +288,116 @@ function Lobby({currUser, currPlayer_table, updatePlayer_table}) {
         
         return (
         <Container>
-            <p>constant updates counter. updates every 5 seconds: {count}</p>
-            <Button id="custombutton" onClick={changelayout}>change layout</Button>
-            
+            {!orderArray[0] ? (
+                <Spinner/>
+            ):(
+                <div>
+                    <p>constant updates counter. updates every 5 seconds: {count}</p>
+                    <Button id="custombutton" onClick={changelayout}>change layout</Button>
 
-            <br></br><br></br>
 
-            {<Modal show={show_too_many_cards} centered animation size="sm" rootClose animation>
-                <Modal.Body id="chosen-role_modal_body" centered>
-                    <p>You fucking donkey. You have {toomanycards} too many card(s).<br></br><br></br> Discard some or play some cards.</p>
-                </Modal.Body>
-                <Modal.Footer id="chosen-role_modal_footer">
-                    <Button id="custombutton" onClick={too_many_cards_okay}>
+                    <br></br><br></br>
+
+                    {<Modal show={show_too_many_cards} centered animation size="sm" rootClose animation>
+                        <Modal.Body id="chosen-role_modal_body" centered>
+                        <p>You fucking donkey. You have {toomanycards} too many card(s).<br></br><br></br> Discard some or play some cards.</p>
+                        </Modal.Body>
+                        <Modal.Footer id="chosen-role_modal_footer">
+                        <Button id="custombutton" onClick={too_many_cards_okay}>
                         Okay
-                    </Button>
-                </Modal.Footer>
-            </Modal>}
+                        </Button>
+                        </Modal.Footer>
+                    </Modal>}
 
-            {<Modal show={show_roledisplay} centered animation size="sm" rootClose animation>
-                <Modal.Header id="chosen-role_modal_header">
-                    <Modal.Title id="chosen-role_modal_header_title" centered><b>Your role:</b></Modal.Title>
-                </Modal.Header>
-                <Modal.Body id="chosen-role_modal_body" centered>
-                    <Image src={role_picture_source} id="chosen-role_modal_body_image"/>
-                </Modal.Body>
-                <Modal.Footer id="chosen-role_modal_footer">
-                    <Button id="custombutton" onClick={roledisplayokay}>
+                    {<Modal show={show_roledisplay} centered animation size="sm" rootClose animation>
+                        <Modal.Header id="chosen-role_modal_header">
+                        <Modal.Title id="chosen-role_modal_header_title" centered><b>Your role:</b></Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body id="chosen-role_modal_body" centered>
+                        <Image src={role_picture_source} id="chosen-role_modal_body_image"/>
+                        </Modal.Body>
+                        <Modal.Footer id="chosen-role_modal_footer">
+                        <Button id="custombutton" onClick={roledisplayokay}>
                         Okay
-                    </Button>
-                </Modal.Footer>
-            </Modal>}
+                        </Button>
+                        </Modal.Footer>
+                    </Modal>}
 
-            {<Modal show={show_rules} centered animation size="sm" rootClose animation>
-                <Modal.Header id="chosen-role_modal_header">
-                    <Modal.Title id="chosen-role_modal_header_title" centered><b>Rules</b></Modal.Title>
-                </Modal.Header>
-                <Modal.Body id="chosen-role_modal_body" centered>
-                    <Image src="/images/back.png" id="chosen-role_modal_body_image"/>
-                </Modal.Body>
-                <Modal.Footer id="chosen-role_modal_footer">
-                    <Button id="custombutton" onClick={closeRules}>
+                    {<Modal show={show_rules} centered animation size="sm" rootClose animation>
+                        <Modal.Header id="chosen-role_modal_header">
+                        <Modal.Title id="chosen-role_modal_header_title" centered><b>Rules</b></Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body id="chosen-role_modal_body" centered>
+                        <Image src="/images/back.png" id="chosen-role_modal_body_image"/>
+                        </Modal.Body>
+                        <Modal.Footer id="chosen-role_modal_footer">
+                        <Button id="custombutton" onClick={closeRules}>
                         Okay
-                    </Button>
-                </Modal.Footer>
-            </Modal>}
-            
+                        </Button>
+                        </Modal.Footer>
+                    </Modal>}
 
-            {<Modal id="choose-role_modal" show={show_rolechoose} centered backdrop="static" keyboard={false} animation>
-                <Modal.Header id="choose-role_modal_header">
-                    <Modal.Title><b>Choose a role card</b></Modal.Title>
-                </Modal.Header>
-                <Modal.Body id="choose-role_modal_body">
-                    <Row>
+
+                    {<Modal id="choose-role_modal" show={show_rolechoose} centered backdrop="static" keyboard={false} animation>
+                        <Modal.Header id="choose-role_modal_header">
+                        <Modal.Title><b>Choose a role card</b></Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body id="choose-role_modal_body">
+                        <Row>
                         <Col id="choose-role_modal_body_row_col">
-                            <Image id="choose-role_modal_body_row_col_image" src="/images/role_cards/back_role.png" onClick={() => addBordertoImage1()}
-                            style={{borderWidth: rolecard_border1}}/>
+                        <Image id="choose-role_modal_body_row_col_image" src="/images/role_cards/back_role.png" onClick={() => addBordertoImage1()}
+                        style={{borderWidth: rolecard_border1}}/>
                         </Col>
                         <Col id="choose-role_modal_body_row_col">
-                            <Image id="choose-role_modal_body_row_col_image" src="/images/role_cards/back_role.png" onClick={() => addBordertoImage2()}
-                            style={{borderWidth: rolecard_border2}}/>
+                        <Image id="choose-role_modal_body_row_col_image" src="/images/role_cards/back_role.png" onClick={() => addBordertoImage2()}
+                        style={{borderWidth: rolecard_border2}}/>
                         </Col>
                         <Col id="choose-role_modal_body_row_col">
-                            <Image id="choose-role_modal_body_row_col_image" src="/images/role_cards/back_role.png" onClick={() => addBordertoImage3()}
-                            style={{borderWidth: rolecard_border3}}/>
+                        <Image id="choose-role_modal_body_row_col_image" src="/images/role_cards/back_role.png" onClick={() => addBordertoImage3()}
+                        style={{borderWidth: rolecard_border3}}/>
                         </Col>
                         <Col id="choose-role_modal_body_row_col">
-                            <Image id="choose-role_modal_body_row_col_image" src="/images/role_cards/back_role.png" onClick={() => addBordertoImage4()}
-                            style={{borderWidth: rolecard_border4}}/>
+                        <Image id="choose-role_modal_body_row_col_image" src="/images/role_cards/back_role.png" onClick={() => addBordertoImage4()}
+                        style={{borderWidth: rolecard_border4}}/>
                         </Col>
-                    </Row>
-                </Modal.Body>
-                <Modal.Footer id="choose-role_modal_footer">
-                    <Button id="custombutton" onClick={chooseRole} disabled={choose_rolecard_disabled}>
+                        </Row>
+                        </Modal.Body>
+                        <Modal.Footer id="choose-role_modal_footer">
+                        <Button id="custombutton" onClick={chooseRole} disabled={choose_rolecard_disabled}>
                         Choose
-                    </Button>
-                </Modal.Footer>
-            </Modal>}
+                        </Button>
+                        </Modal.Footer>
+                    </Modal>}
 
-            {<Modal show={show_turn_starts} centered animation size="sm" rootClose animation>
-                <Modal.Header id="chosen-role_modal_header">
-                    <Modal.Title id="chosen-role_modal_header_title" centered><b>Your Turn</b></Modal.Title>
-                </Modal.Header>
-                <Modal.Body id="chosen-role_modal_body" centered>
-                    <Image src="/images/back.png" id="chosen-role_modal_body_image"/>
-                </Modal.Body>
-                <Modal.Footer id="chosen-role_modal_footer">
-                    <Button id="custombutton" onClick={closeDrawnCards}>
+                    {<Modal show={show_turn_starts} centered animation size="sm" rootClose animation>
+                        <Modal.Header id="chosen-role_modal_header">
+                        <Modal.Title id="chosen-role_modal_header_title" centered><b>Your Turn</b></Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body id="chosen-role_modal_body" centered>
+                        <Image src="/images/back.png" id="chosen-role_modal_body_image"/>
+                        </Modal.Body>
+                        <Modal.Footer id="chosen-role_modal_footer">
+                        <Button id="custombutton" onClick={closeDrawnCards}>
                         Okay
-                    </Button>
-                </Modal.Footer>
-            </Modal>}
+                        </Button>
+                        </Modal.Footer>
+                    </Modal>}
 
-            <LayoutSwitcher playeramount={playeramount} playertable={currPlayer_table} orderarray={orderArray} visibility={hidden_gamefield} player_id={currUser.id}/>
-            {/*<LayoutSwitcher playeramount={playeramount} visibility={hidden_gamefield}/>*/}
+                    <LayoutSwitcher playeramount={playeramount} playertable={currPlayer_table} orderarray={orderArray} visibility={hidden_gamefield} player_id={currUser.id}/>
+                    {/*<LayoutSwitcher playeramount={playeramount} visibility={hidden_gamefield}/>*/}
 
-            <OverlayTrigger trigger="click" overlay={role_information} rootClose>
-                <Button id="custombutton" >Show role information</Button>
-            </OverlayTrigger>
-            <Button variant={ready_button_color} onClick={toggleReady} hidden={hidden_startgame}>{ready_button_text}</Button>
-            <Button onClick={endTurn} id="custombutton">End Turn</Button>
-            <Button onClick={resign} id="custombutton">Resign</Button>
-            <Button onClick={openRules} id="custombutton">Rules</Button>
-            <Button onClick={leaveGame} id="custombutton">Leave</Button>
-            <br></br>
-            <br></br>
+                    <OverlayTrigger trigger="click" overlay={role_information} rootClose>
+                    <Button id="custombutton" >Show role information</Button>
+                    </OverlayTrigger>
+                    <Button variant={ready_button_color} onClick={toggleReady} hidden={hidden_startgame}>{ready_button_text}</Button>
+                    <Button onClick={endTurn} id="custombutton">End Turn</Button>
+                    <Button onClick={resign} id="custombutton">Resign</Button>
+                    <Button onClick={openRules} id="custombutton">Rules</Button>
+                    <Button onClick={leaveGame} id="custombutton">Leave</Button>
+                    <br></br>
+                    <br></br>
+                </div>
+            )}
         </Container >
     );
 }
