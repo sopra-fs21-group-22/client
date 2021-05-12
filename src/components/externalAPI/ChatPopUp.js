7111fd9f4ff47e21ffaaca2c3cd7c80f7779bef3
Elexa Heggli <elexa.heggli@gmx.ch>
2021-05-13 00:29:41 +0200

    continued chat

--- src/components/externalAPI/ChatPopUp.js
+++ src/components/externalAPI/ChatPopUp.js
@@ -2,10 +2,17 @@ import {Button, Card, Form, FormControl, InputGroup} from "react-bootstrap";
 import {authApi} from "../../helpers/api";
 import React, {useState} from "react";
 
+
 function ChatPopUp({chat, player, playertable}) {
 
+    /*
     const messages = chat.map((m) =>
-        <li key={m.id}>m.content</li>
+        <li key={m.id}>{m.name + ": " + m.content}</li>
+    );
+*/
+    const arr = [{id: 1, name: "Mario", content: "I like hats"}, {id: 2, name: "Nad", content: "I like cats"}, {id: 3, name: "a", content: "I like mats"}, {id: 4, name: "b", content: "I like tatts"}, {id: 5, name: "c", content: "I like pats"}, {id: 6, name: "d", content: "I like rats"}]
+    const messages = arr.map((m) =>
+        <p key={m.id}>{m.name + ": " + m.content + "\n"}</p>
     );
 
     function sendMessage(message) {
@@ -17,13 +24,15 @@ function ChatPopUp({chat, player, playertable}) {
         //authApi().post(`/games/${playertable.id}/players/${player.id}/chat`, requestBody)
     }
 
+
     const [message, setMessage] = useState("");
 
     return(
-        <Card style={{backgroundColor: "none", opacity: 0.8}}>
-        <Card.Header>Chat</Card.Header>
+        <Card class="overflow-auto" style={{backgroundColor: "none", opacity: 0.8, maxHeight: 200}}>
+
+            <Card.Header>Chat</Card.Header>
         <Card.Body>
-            <ul> {messages} </ul>
+            <div> {messages} </div>
             <InputGroup className="mb-3">
                 <FormControl
                     placeholder="Message"
@@ -32,12 +41,13 @@ function ChatPopUp({chat, player, playertable}) {
                     onChange={m => setMessage(m.target.value)}
                 />
                 <InputGroup.Append>
-                    <Button id="custombutton" variant="outline-secondary" onClick={sendMessage(message)}>Send</Button>
+                    <Button id="custombutton" variant="outline-secondary" onClick={sendMessage(message) && this.target.value === ""}>Send</Button>
                 </InputGroup.Append>
             </InputGroup>
         </Card.Body>
     </Card>
     );
 }
+
 
 export default ChatPopUp;
