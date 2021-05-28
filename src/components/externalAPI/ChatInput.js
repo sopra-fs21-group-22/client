e092b025d2fca0f1f9ec46ad1df5b236dc7bfbf7
Elexa Heggli <elexa.heggli@gmx.ch>
2021-05-28 16:39:30 +0200

    all new messages are read outloud with toasts

--- src/components/externalAPI/ChatInput.js
+++ src/components/externalAPI/ChatInput.js
@@ -49,57 +49,19 @@ class ChatInput extends Component {
 
     async sendMessage() {
         this.state.sending = true;
-        console.log("before", this.state.sending);
         try {
             const requestBody = JSON.stringify({
                 "content": this.state.value,
                 "name": this.props.player.user,
             });
             await authApi().put(`/games/${this.props.playertable.id}/players/${this.props.player.id}/chat`, requestBody);
-            await this.characterToSpeech();
             this.setState({value: ""});
             console.log("player: ", this.props.player);
         } catch (error) {
             console.warn(error);
         } finally {
             this.state.sending = false;
-            console.log("before", this.state.sending);
         }
-    }
-
-    async characterToSpeech(){
-        let character_response = await authApi().get(`/games/${this.props.playertable.id}/players/${this.props.player.id}/characters`);
-        const characterName = character_response.data.name;
-
-        /*  const voices=["en-PH-JamesNeural", "en-US-GuyNeural", "en-GB-LibbyNeural", "en-IE-ConnorNeural", "en-CA-LiamNeural", "en-IN-PrabhatNeural", "en-AU-NatashaNeural"];
-            const characters=["elgringo", "willythekid", "rosedoolan", "paulregret", "jourdonnais", "bartcassidy", "suzylafayette"]; */
-        let voice = "";
-        switch(characterName){
-            case "elgringo":
-                voice = "en-PH-JamesNeural";
-                break;
-            case "willythekid":
-                voice = "en-US-GuyNeural";
-                break;
-            case "rosedoolan":
-                voice = "en-GB-LibbyNeural";
-                break;
-            case "paulregret":
-                voice = "en-IE-ConnorNeural";
-                break;
-            case "jourdonnais":
-                voice = "en-CA-LiamNeural";
-                break;
-            case "bartcassidy":
-                voice = "en-IN-PrabhatNeural";
-                break;
-            case "suzylafayette":
-                voice = "en-AU-NatashaNeural";
-                break;
-        }
-        synthesizeSpeech(voice, this.state.value);
-        console.log("voice: ", voice);
-        console.log("character: ", characterName);
     }
 
     handleChange(e) {
